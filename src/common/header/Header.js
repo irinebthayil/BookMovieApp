c1bf1ab81d00d063ba72efb9a78102843d9adf99
irinebthayil <irinebthayil@gmail.com>
2022-09-28 01:04:36 +0530

    Login, Logout and Book Show buttons

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -4,14 +4,15 @@ import logo from "../../assets/logo.svg";
 import Button from '@material-ui/core/Button';
 
 function Header() {
+
     return (
         <div className="header">
             <img src={logo} alt="Logo" className="logo"></img>
             <div className="header-buttons-div">
                 <Button id="bookShowButton" variant="contained" color="primary">Book Show</Button>
-                <Button id="loginLogoutButton" variant="contained">Login</Button>
+                <Button id="loginButton" variant="contained">Login</Button>
+                <Button id="logoutButton" variant="contained">Logout</Button>
             </div>
-            
         </div>
     )
 }
